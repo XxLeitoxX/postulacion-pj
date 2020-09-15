3e807f6a0f88e0f01ca41c52e7d325bb0c846e08
Manuel Sayago <manuelsayago15@gmail.com>
2020-09-14 21:24:29 -0300

    Adding mapState and mapMutations and input fix with class active when typing

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { mapState } from 'vuex'
+import { validaRut, telValidate, emailValidate } from "./../validation/validation";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
   	active: '',
-  	activeRequest: '',
-	activeRecovery: '',
+  	rutIsValid: true
 	  
   },
   mutations: {
   	focus(state, refs) {
   		refs.className = "input active"
-  		console.log(refs);
+  		//console.log(refs);
+  		console.log("test");
   	},
 
   	blur(state, refs) {
-  		refs.className = "input"
-  		console.log(refs);
+  		if (refs[1] == "") {
+
+  			refs[0].className = "input"
+  		} else {
+  			refs[0].className = "input active"
+  		}
+  		//console.log(refs);
+  		//console.log(refs[1]);
   		//state.active = ''
   	},
+
+  	validation(state, rut, tel, email, emailConfirm) {
+
+      if (rut !== '') {
+        console.log(rut);
+        console.log(state.rutIsValid);
+        validaRut(rut) ? state.rutIsValid = true : state.rutIsValid = false; 
+      }
+
+      if (this.tel !== '') {
+        telValidate(this.tel) ? this.telIsValid = true : this.telIsValid = false;
+      }
+
+      if (this.email !== '') {
+        emailValidate(this.email) ? this.emailIsValid = true : this.emailIsValid = false;
+      }
+
+      if (this.emailConfirm !== '') {
+         emailValidate(this.emailConfirm) ? this.emailConfirmIsValid = true : this.emailConfirmIsValid = false;
+      }
+    },
 	
 
   },
@@ -28,7 +57,7 @@ export default new Vuex.Store({
   },
   modules: {
 
-  }
+  },
 
   
 })
