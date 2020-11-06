6cbabbc022467e5d58dbaebb38ee4ddfa6b4d00f
Manuel Sayago <manuelsayago15@gmail.com>
2020-11-06 15:07:55 -0300

    dist vue config again 2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-publicPath: process.env.NODE_ENV === 'production'
+/*publicPath: process.env.NODE_ENV === 'production'
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'? '/postulacion_online_frontend2/dist/': '/'
-}
+}*/
