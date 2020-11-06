ebe97828f086f870b2be72da581e131134ba8081
Manuel Sayago <manuelsayago15@gmail.com>
2020-11-06 14:20:00 -0300

    changes vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 publicPath: process.env.NODE_ENV === 'production'
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'? '/postulacion/': '/'
+	publicPath: process.env.NODE_ENV === 'production'? '/postulacion_online_frontend2/': '/'
 }
