21db0af4fe2b79464ecdca6efe87c1c00b0b085a
Leonardo <lgimenez@corp.isc.cl>
2020-12-02 00:29:44 -0300

    correcciones leo

--- src/views/RecoverySuccess.vue
+++ src/views/RecoverySuccess.vue
@@ -9,7 +9,7 @@
                 <div class="info">
                   <h2>CÓDIGO ENVIADO</h2>
                   <p>El código ha sido enviado a tu correo {{ emailRecoveryCodeGlobal }} y debería llegar en unos minutos.</p>
-                  <p class="u-mb50">Recuerda la carpeta de SPAM.</p><a class="btn-blue" @click="home">INGRESAR<i class="fa fa-angle-right"></i></a><a class="recovery" @click="recoveryCode">Solicitar envío de código nuevamente</a>
+                  <p class="u-mb50">Recuerda revisar la carpeta de SPAM.</p><a class="btn-blue" @click="home">INGRESAR<i class="fa fa-angle-right"></i></a><a class="recovery" style="cursor: pointer" @click="recoveryCode">Solicitar envío de código nuevamente</a>
                 </div>
               </div>
             </div>
